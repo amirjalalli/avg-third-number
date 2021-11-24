767a144a4606e4fe2c9240f1a52c65079d7258f3
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-24 18:12:12 +0330

    if-if-if

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-
-let yourNumber = prompt("Enter a number between 0 to 20");
-if (yourNumber > 10){
+var yourMark = prompt("enter your number between 0 to 20")
+if (yourMark < 0){
+    alert("your number not right")
+}else if(yourMark > 20){
+    alert("your number not right")
+}else if(yourMark > 10){
     alert("your number great than 10")
 }else{
     alert("your number not great than 10")
