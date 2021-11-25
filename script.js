b8a8d66abdb90ae945882ba2eae0b6a55d7c10a7
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-25 11:33:22 +0330

    to-change-string-to-number

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-var date = new Date()
-var today = date.getDay()
-switch(today){
-    case 1 :
-        document.write("Mon")
-        break;
-    case 2 :
-        document.write("Tus")
-        break;
-    case 3 :
-        document.write("Wen")
-        break;
-    case 4 :
-        document.write("tur")
-        break;
-    case 5 :
-        document.write("Fri")
-        break;
-    case 6 : 
-        document.write("Sat")
-         break; 
-    case 7 : 
-        document.write("Sun")
-        break;
-    default:document.write("error")  
-}
+var userAge = prompt("enter your age:");
+var userNumberAge = +(userAge);
+alert(typeof userNumberAge);
