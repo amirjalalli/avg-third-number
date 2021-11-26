66405c8fbec1886d326c633bca7fbcfa4c897e84
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-11-26 10:22:40 +0330

    first-opearator

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-var str1 = "JavaScript";
-var str2 = "Amir jalali";
-var num1 = 15;
-var num2 = 5;
-
-alert(isNaN(str1 - num2));//====>true
-alert(isNaN(num1 / num2));//=======false
+var num1 = 12;
+var num2 = 4;
+var num3 = 3;
+var str1 = "3";
+// alert(num2 == num3);// 4 == 3 ==> false
+// alert(num2 * num3 == num1);//3*4 == 12==>true
+// alert(num1 > num2)//12>4 ==> true
+alert(str1 === num3);// "3"===3===>flase
